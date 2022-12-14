95701b471529ce5fe12d04829602e00e658bacb1
Mead Solomon <mead93@gmail.com>
2022-12-14 11:31:34 -0600

    added conditional to verify that phone is 10 digits and to display error message if not

--- main.js
+++ main.js
@@ -25,6 +25,14 @@ const submit = (e) => {
     errorMessage.textContent = "Passwords don't match";
     return;
   }
+  if (
+    !phone.value.match(
+      /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
+    )
+  ) {
+    phone.classList.add("error");
+    errorMessage.textContent = "Phone number must be 10 digits";
+  }
 };
 
 const form = document.querySelector("form");
