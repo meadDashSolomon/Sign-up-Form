f9a7e7a5f441774c17935b0745628da5ea466010
Mead Solomon <mead93@gmail.com>
2022-12-14 12:04:21 -0600

    added password length, lowercase, uppercase, and number verification and error messages

--- main.js
+++ main.js
@@ -32,7 +32,31 @@ const submit = (e) => {
   ) {
     phone.classList.add("error");
     errorMessage.textContent = "Phone number must be 10 digits";
+    return;
   }
+  if (password.value.length < 8) {
+    password.classList.add("error");
+    errorMessage.textContent = "Password must be at least 8 characters long.";
+    return;
+  }
+  if (!password.value.match(/[a-z]/)) {
+    password.classList.add("error");
+    errorMessage.textContent =
+      "Password must have at least one lowercase letter.";
+    return;
+  }
+  if (!password.value.match(/[A-Z]/)) {
+    password.classList.add("error");
+    errorMessage.textContent =
+      "Password must have at least one uppercase letter.";
+    return;
+  }
+  if (!password.value.match(/\d+/g)) {
+    password.classList.add("error");
+    errorMessage.textContent = "Password must have at least one number.";
+    return;
+  }
+  errorMessage.textContent = "Form added successfully!";
 };
 
 const form = document.querySelector("form");
